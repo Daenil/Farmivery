9fa9a5ad5183e4a781d5d6096853ad3b7da29b28
Rafaelx32 <117119646+Rafaelx32@users.noreply.github.com>
2023-12-03 19:25:16 -0300

    Acrescentei a tabela Itens_Pedidos e coloquei o id da farmacia nas tabelas entregadores e Farmaceuticos

--- Farmivery.sql
+++ Farmivery.sql
@@ -40,17 +40,17 @@ create table Farmaceuticos
 	idFarmacia			int				not null		references	Farmacias(farmaciaId),
 	nomeFarmaceutico	varchar(100)	not null,
 	telefone			varchar(20)		not null		unique,
-	primary key(farmaceuticoId, idFarmacia)		
+	primary key(farmaceuticoId, idFarmacia),
 )
 
 --Tabela Entregadores
 create table Entregadores
 (
-	entregadorId	int					not null		identity,
+	entregadorId	int					not null		Primary Key identity,
 	idFarmacia		int					not null		references	Farmacias(farmaciaId),
 	nomeEntregador	varchar(100)		not null,
 	telefone		varchar(20)			not null		unique,
-	primary key(entregadorId, idFarmacia)
+	unique(entregadorId, idFarmacia)
 )
 
 
@@ -83,13 +83,23 @@ create table Produtos
 --Tabela Pedidos
 create table Pedidos
 (
-	pedidoId INT NOT NULL primary key IDENTITY,
-	ped_data DATETIME,
-	ped_valor DECIMAL(10, 2) CHECK (ped_valor > 0),
-	status INT NULL CHECK (status IN (1, 2, 3)),
-	idCli int not null references Clientes(clienteId),
+	pedidoId		INT				NOT NULl	Primary Key	IDENTITY,
+	ped_data		DATETIME,
+	ped_valor		DECIMAL(10, 2)	CHECK(ped_valor > 0),
+	status			INT				NULL CHECK(status IN (1, 2, 3)),
+	idCli			int				not null	references	Clientes(clienteId),
+	idEntregador	int				not null	references	Entregadores(entregadorId),
 )
 
+--Tabela itens_pedidos
+create table itens_Pedidos
+(
+	idCodigo	int		not null	references Pedidos(pedidoId),
+	idProduto	int		not null	references Produtos(produtoId),
+	itp_qtd		int		not null	check(itp_qtd > 0),
+	itp_valor	money	not null	check(itp_valor > 0), 
+	primary key(idCodigo, idProduto)
+)
 
 ----------------------------------------------------------------------
 -- procedures
@@ -107,3 +117,82 @@ begin
 end
 go
 
+-- 2 - Create Procedure Cadastrar Farmaceutico
+create Procedure sp_CadFarmaceuticos
+(
+	@idFarmacia int, @nomeFarmaceutico varchar(50), @telefoneFarmaceutico varchar(20)
+)
+as
+begin
+	insert into Farmaceuticos (idFarmacia, nomeFarmaceutico, telefone)
+	values (@idFarmacia, @nomeFarmaceutico, @telefoneFarmaceutico)
+end
+go
+
+-- 3 - Create Procedure Cadastrar Entregadores
+create Procedure sp_CadEntregadores
+(
+	@idFarmacia int, @nomeEntregador varchar(50), @telefoneEntregador varchar(20)
+)
+as
+begin
+	
+	insert into Entregadores (idFarmacia, nomeEntregador, telefone)
+	values (@idFarmacia, @nomeEntregador, @telefoneEntregador)
+end
+go
+
+
+----------------------------------------------------------------------------------------
+-- Views
+----------------------------------------------------------------------------------------
+-- 1 - create view farmacias e farmaceuticos
+create view v_Farmacias_Farmaceuticos
+as
+	select f.farmaceuticoId, f.idFarmacia,  F.nomeFarmaceutico, f.telefone
+	from Farmacias far inner join Farmaceuticos F on Far.farmaciaId = f.idFarmacia
+go
+-- Testando View
+select * from v_Farmacias_Farmaceuticos
+
+-- 2 - create view Farmacias e Entregadores
+create view v_Farmacias_Entregadores
+as
+	select e.entregadorId, e.idFarmacia, e.nomeEntregador, e.telefone
+	from Farmacias F inner join Entregadores E on F.farmaciaId = E.idFarmacia
+go
+-- Testando View
+select * from v_Farmacias_Entregadores
+
+
+--3 - View Itens_Pedidos
+create view v_Itens_Pedidos
+as
+	select ip.idCodigo [Código_Pedido], ip.idProduto Código_Produto, Pro.nome [Nome_Produto], ip.itp_qtd 'Quantidade', ip.itp_valor Valor_Pedido
+	from itens_Pedidos IP 
+	inner join 
+	Pedidos P  on ip.idCodigo = P.pedidoId 
+	inner join 
+	Produtos Pro on ip.idProduto = pro.produtoId
+go
+-- Testando View
+select * from v_Itens_Pedidos
+
+-- 4 - View Pedidos
+create View v_Pedidos
+as
+	select	p.pedidoId Código_Pedido, P.idCli Código_Cliente, c.nomeCliente Nome_Cliente, p.idEntregador Código_Entregador, e.nomeEntregador[Nome_Entregador], p.ped_valor 'Valor', p.ped_data [Data_Pedido],
+			case status
+						when 1 then 'Andamento'
+						when 2 then 'Entregue'
+						when 3 then	'Cancelado'
+						else		'Indisponível'
+					end 'situação'
+	from Pedidos P
+	inner join
+	Entregadores E on p.idEntregador = E.entregadorId
+	inner join
+	Clientes C on p.idCli = C.clienteId
+go
+-- Testando View 
+select * from v_Pedidos
